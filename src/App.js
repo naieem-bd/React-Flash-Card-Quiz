9379b1739f18e2939678da5fe7bf758edc6fec19
naieem-bd <naieemur@gmail.com>
2021-11-23 16:27:56 +0600

    add flashcard and style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React, { useState } from 'react'
+import FlashCardList from './components/FlashcardList'
+import './style.css'
+
 
 function App() {
+  const [flashcards, setFlashcards] = useState(sampleData)
+
   return (
-    <p>test</p>
+    <FlashCardList flashcards={flashcards} />
   );
 }
+
+const sampleData = [
+  {
+    id: 1,
+    question: 'dummy question 1',
+    answer: 'ans 1',
+    options: ['one', 'two', 'three', 'four']
+  },
+  {
+    id: 2,
+    question: 'question number two',
+    answer: 'second ans',
+    options: ['one', 'two', 'three', 'four']
+  },
+  {
+    id: 3,
+    question: 'i am question 3',
+    answer: '3 ans',
+    options: ['sunlight', 'moonlight', 'light', 'four']
+  }
+]
 
 export default App;
